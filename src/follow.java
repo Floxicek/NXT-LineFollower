8f01f3fb479aecd85c40acc976efaa88b6fe0499
Floxicek <jan@ejhle.net>
2024-11-05 09:10:25 +0100

    backup

--- src/follow.java
+++ src/follow.java
@@ -1,43 +1,45 @@
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
+import lejos.util.Delay;
 import lejos.nxt.LightSensor;
 
 public class follow {
 	static int white;
 	static int black;
 	static LightSensor ls = new LightSensor(SensorPort.S4);
-	
-	static int baseSpeed = 300;
-	static int kp = 215; // reg constant
-	static int ki = 0; // reg constant
-	static int kd = 240; // reg constant
-	
+
+	static int baseSpeed = 320;
+	static float kp = 0.67f; // reg constant
+	static float ki = 0f; // reg constant
+	static float antiWindup = 5/100f;
+	static float kd = 0.1f; // reg constant
+
 	static float colorMid;
 	static int colorDiff;
-	
-	public static void getInitialValues() { 
-		 System.out.println("Place on black"); 
-		 Button.waitForAnyPress();
-		 black = ls.getNormalizedLightValue();
-		 
-		 Motor.A.setSpeed(80);
-		 Motor.B.setSpeed(80);
-		 
-		 Motor.A.rotate(150, true);
-		 Motor.B.rotate(-150);
-		 
-		 white = ls.getNormalizedLightValue();
-		 
-		 System.out.println(black);
-		 System.out.println(white);
-		 
-		 Motor.A.rotate(-150, true); // 90 deg robot rotation
-		 Motor.B.rotate(150);
-		 colorMid = (float)(white + black)/2.0f;
-		 colorDiff = white - black;
+
+	public static void getInitialValues() {
+		System.out.println("Place on black");
+		Button.waitForAnyPress();
+		black = ls.getNormalizedLightValue();
+
+		Motor.B.setSpeed(160);
+
+		Motor.B.rotate(-200);
+//		Motor.B.rotate(-150);
+
+		white = ls.getNormalizedLightValue();
+
+		System.out.println(black);
+		System.out.println(white);
+
+		Motor.B.rotate(200); // 90 deg robot rotation
+//		Motor.B.rotate(150);
+		colorMid = (float) (white + black) / 2.0f;
+		colorDiff = Math.abs(white - black);
+//		 Button.waitForAnyPress();
 	}
-	
+
 	public static float getError() {
 //		-1 to 1
 		int lightValue = ls.getNormalizedLightValue();
@@ -47,56 +49,68 @@ public class follow {
 			return -1;
 		}
 //		white > black
-		
-		float err = (float)(lightValue - colorMid) / (float)(colorDiff/2.0f);
+
+		float err = (float) (lightValue - colorMid) / (float) (colorDiff / 2.0f);
 		return err;
 	}
-	
+
 	public static void print(String str) {
 		System.out.println(str);
 	}
-	
+
 	static float i;
 	static float previousErr;
-	public static void regulateSpeed() {
-		float error = getError();
-		
-		float p = error;
-		i += error;
-		float d = error - previousErr;
-		previousErr = error;
-		
-		
-		float correction = p * kp + i * ki + d*kd;
-		
-//		System.out.println(getColorError());
-		 
-		 
-		 Motor.A.setSpeed(baseSpeed + correction);
-		 Motor.B.setSpeed(baseSpeed - correction);
-		 
-		 
-		 
-		 
-		 
-	}
+	static float lastTime;
 	
-	 public static void main (String[] args) 
-	 { 
-		 getInitialValues();
-		 Motor.A.backward();
-		 Motor.B.backward();
-		 
-		 while(!Button.ENTER.isDown()){			 
+	public static float getDeltaTime() {
+		float delta = (System.currentTimeMillis()-lastTime)/1000f;
+		lastTime = System.currentTimeMillis();
+		return delta;
+	}
+
+	public static void regulateSpeed() {
+		float delta = getDeltaTime();
+		
+		float error = getError();
+
+		float p = error;
+		i += error * delta;
+		i = Math.min(Math.max(i, -antiWindup), antiWindup);
+		
+		float d = (float)(error - previousErr)/delta;
+		previousErr = error;
+
+		float correction = p * kp + i * ki + d * kd;
+		setSpeed(baseSpeed + baseSpeed * correction, baseSpeed - baseSpeed * correction);
+//		System.out.println(error);
+		//System.out.printf("E %.2f I %.2f\n", error, i);
+		System.out.println(d);
+		
+	}
+	static boolean aForward = true;
+	static boolean bForward = true;
+
+	public static void setSpeed(float left, float right) {
+		Motor.A.setSpeed(Math.max(right, 1));
+		Motor.B.setSpeed(Math.max(left, 1));
+	}
+
+	public static void main(String[] args) {
+		getInitialValues();
+		Motor.A.backward();
+		Motor.B.backward();
+
+		while (!Button.ENTER.isDown()) {
 //		 	MAIN LOOP
-			 regulateSpeed();
-		 }
-		 Motor.A.stop();
-		 Motor.B.stop();
-		 
-		 for (int i = 0; i < 10; i++) {
-			System.out.println(" ");
+			regulateSpeed();
+			Delay.msDelay(10);
 		}
-		 Button.waitForAnyPress();
-		 } 
+		Motor.A.stop();
+		Motor.B.stop();
+
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println(" ");
+//		}
+//		 Button.waitForAnyPress();
+	}
 }
